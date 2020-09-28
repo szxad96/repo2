68e8884d5ddcfaa089f6dc3e60cea4d21f12621d
suzexuan <suzexuan@jd.com>
2020-09-28 20:15:12 +0800

    品牌新增

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/BrandController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -4,12 +4,16 @@ import com.leyou.item.service.BrandService;
 import com.szx.common.pojo.PageResult;
 import com.szx.item.pojo.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("brand")
@@ -40,6 +44,10 @@ public class BrandController {
         }
 
         return ResponseEntity.ok(result);
-
+    }
+    @PostMapping
+    public ResponseEntity<Void> saveBrand(Brand brand, @RequestParam("cids") List<Long> cids){
+        this.brandService.saveBrand(brand, cids);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/BrandService.java
@@ -8,6 +8,7 @@ import com.szx.item.pojo.Brand;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class BrandService {
 
     /**
      * 根据分页条件分页并排序查询品牌信息
+     *
      * @param key
      * @param page
      * @param rows
@@ -32,19 +34,37 @@ public class BrandService {
         Example.Criteria criteria = example.createCriteria();
 
         //根据name模糊查询，根据首字母查询
-        if (StringUtils.isNotBlank(key)){
-            criteria.andLike("name", "%" + key + "%").orEqualTo("letter",key);
+        if (StringUtils.isNotBlank(key)) {
+            criteria.andLike("name", "%" + key + "%").orEqualTo("letter", key);
         }
         //添加分页条件
-        PageHelper.startPage(page,rows);
+        PageHelper.startPage(page, rows);
         //添加排序条件
-        if (StringUtils.isNotBlank(sortBy)){
-            example.setOrderByClause(sortBy + " " + (desc ? "desc": "asc"));
+        if (StringUtils.isNotBlank(sortBy)) {
+            example.setOrderByClause(sortBy + " " + (desc ? "desc" : "asc"));
         }
         List<Brand> brands = this.brandMapper.selectByExample(example);
         PageInfo<Brand> pageInfo = new PageInfo<>(brands);
         //包装成分页结果集返回
         return new PageResult<>(pageInfo.getTotal(), pageInfo.getList());
+
+    }
+
+    /**
+     * 新增品牌
+     *
+     * @param brand
+     * @param cids
+     */
+    @Transactional
+    public void saveBrand(Brand brand, List<Long> cids) {
+        //先新增brand，再新增中间表
+        this.brandMapper.insertSelective(brand);
+        //新增中间表
+
+        for (Long cid : cids) {
+            this.brandMapper.isnertCategoryAndBrand(cid, brand.getId());
+        }
 
     }
 }
